6933d21a3a028537da605ddf59c015b61bcebb7c
wfalby4257 <walter.falby@bsu.edu>
2025-01-19 22:43:20 +0000

    Versioning the project

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,13 @@
+namespace FactorialLibrary.Tests;
+
+using FactorialLibrary;
+public class FactorialServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = FactorialService.Factorial(0);
+        Assert.Equal(1, result);
+
+    }
+}
